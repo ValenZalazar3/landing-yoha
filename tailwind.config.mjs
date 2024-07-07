daeda05965a1605374e5c9d553d2b110b5e13d56
Valen <valenzalazar3@gmail.com>
2024-07-06 22:55:08 -0300

    last changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {boxShadow: {
-			'up-green': '0 -8px 18px -1px rgba(0, 230, 118, 0.7)',
+			'up-green': '0 -2px 10px 0px rgba(0, 230, 118, 0.7)',
 			'up-green-hover': '0 -12px 20px -6px rgba(0, 230, 118, 0.9)',
 		  },},
 	},
